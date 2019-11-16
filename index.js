dc566037249117c8e195954dd66898d9b732aeab
Francisco <fpronto94@gmail.com>
2019-11-16 15:17:19 +0000

    added some comments

--- index.js
+++ index.js
@@ -5,31 +5,32 @@ const config = JSON.parse(fs.readFileSync('./config.json'));
 const client  = mqtt.connect(config.url);
  
 client.on('connect', () => {
-  client.subscribe('#', (err) => {});
+  client.subscribe('#', () => {});
 });
  
 client.on('message', (topic, message) => {
   const parsedMsg = JSON.parse(message);
-
-  if (topic === 'question' && parsedMsg.message_type === 'round_end') {
-    console.log(message.toString());
-  }
-  if (topic === 'question' && parsedMsg.message_type === 'round_start') {
-    console.log(message.toString());
-    const sendObj = {
-      message_type: 'round_answer',
-      question_id: parsedMsg.question_id,
-      user_id: config.user_id,
-      user_token: config.user_token,
-      answer: 0
-    };
-    
-    //publish 3x
-    for (let i = 0; i < config.num_requests; i++) {
-      for (let j = 0; j < config.num_answers; j++) {
-        sendObj.answer = j;
-        client.publish('answer', JSON.stringify(sendObj));
-      }
+  if (topic === 'question') {
+    switch (parsedMsg.message_type) {
+      case 'round_start':
+          const sendObj = {
+            message_type: 'round_answer',
+            question_id: parsedMsg.question_id,
+            user_id: config.user_id, // this doesn't change
+            user_token: config.user_token, // this is changing for every login, needs a fix
+            answer: 0
+          };
+          
+          //publish 3x
+          for (let i = 0; i < config.num_requests; i++) {
+            for (let j = 0; j < config.num_answers; j++) {
+              sendObj.answer = j;
+              client.publish('answer', JSON.stringify(sendObj));
+            }
+          }
+      default:
+        console.log(message.toString());
+        break;
     }
   }
 });
